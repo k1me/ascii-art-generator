7975099eb9d92d16aec485ef3ccf88cfd6fa5b5c
k1me <m.kiraly21@proton.me>
2024-07-22 08:16:06 +0200

    added kinda working edge detection

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-import math, time
+import math, time, cv2
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
@@ -12,16 +12,54 @@ def get_luminance(img, case=False):
 
 
 def get_char(luminance, interval, chars):
-    index = (math.floor(luminance * interval) - 1) % len(chars)
+    index = (math.floor(luminance * interval)) % len(chars)
     return chars[index]
 
 
+def detect_edges(img):
+    grayscale = cv2.cvtColor(np.array(img), cv2.COLOR_RGBA2GRAY)
+    img_gx = cv2.Sobel(
+        grayscale, cv2.CV_32FC1, 1, 0, ksize=5, borderType=cv2.BORDER_DEFAULT
+    )
+    img_gy = cv2.Sobel(
+        grayscale, cv2.CV_32FC1, 0, 1, ksize=5, borderType=cv2.BORDER_DEFAULT
+    )
+    img_grad_magnitude = cv2.magnitude(img_gx, img_gy)
+    normalized = cv2.normalize(
+        img_grad_magnitude, None, 0, 255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1
+    )
+    threshold = 50
+    _, img_thresholded = cv2.threshold(normalized, threshold, 255, cv2.THRESH_BINARY)
+    return img_thresholded, img_gx, img_gy
+
+
+def compute_angles(img, gx, gy):
+    angles = np.full_like(img, fill_value=None, dtype=object)
+    for y in range(img.shape[0]):
+        for x in range(img.shape[1]):
+            if img[y, x] >= 0:
+                angle_rad = math.atan2(gx[y, x], gy[y, x])
+                angle_deg = int(math.degrees(angle_rad))
+                angles[y, x] = angle_deg
+    return angles
+
+
+def get_edge_char(angle):
+    if (75 < angle < 105) or (-95 < angle < -85):
+        return "|"
+    if (-15 < angle < 15) or (angle > 165) or (angle < -165):
+        return "—"
+    if (-15 <= angle < 75) or (-165 < angle < -95):
+        return "/"
+    return "\\"
+
+
 def main():
-    chars = " `.-':_,^=;><+rcz?LT)JfIneoZ5Y2SkP6OX0%&@"
+    chars = " .;coPO?@■"
     char_list = list(chars)
     char_width = 8
     char_height = 12
-    scale_factor = 1 / 8
+    scale_factor = 1 / 4
     font_path = "lucon.ttf"
     output_image_path = "output.png"
     image_path = input("Enter the path to the image:\n")
@@ -57,12 +95,17 @@ def main():
     )
     draw = ImageDraw.Draw(output_img)
     interval = len(char_list) / get_luminance(img, case=True)
+    edges, img_gx, img_gy = detect_edges(img)
+    angles = compute_angles(edges, img_gx, img_gy)
 
     for y in range(pixels.shape[0]):
         for x in range(pixels.shape[1]):
-            lum = luminance[y, x]
+            if edges[y, x] > 0:
+                char = get_edge_char(angles[y, x])
+            else:
+                lum = luminance[y, x]
+                char = get_char(lum, interval, char_list)
             r, g, b = pixels[y, x]
-            char = get_char(lum, interval, chars)
             draw.text((x * char_width, y * char_height), char, font=fnt, fill=(r, g, b))
 
     output_img.save("output.png")
